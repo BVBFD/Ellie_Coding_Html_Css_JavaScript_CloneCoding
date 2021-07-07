6897f1601f219a25e163fc0b35c4779bac68a9e6
BVBFD <lsevina126@gmail.com>
2021-07-07 17:15:12 +0900

    JS: NavbarMenu

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 'use strict';
 
 // Make navbar transparent when it is on the top
-// const navbar = document.querySelector('#navbar');
-// const navbarHeight = navbar.getBoundingClientRect().height;
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () =>{
     // console.log(window.scrollY);
     // console.log(`navbarHeight: ${navbarHeight}`);
@@ -15,3 +15,20 @@ document.addEventListener('scroll', () =>{
 
 // 내가 스크롤을 할때마다 내가 등록한 이 arrow fuction을 호출해줘.
 // https://developer.mozilla.org/ko/docs/Web/API/Window/scrollY
+
+// Handle scrolling when tapping on the navbar menu
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+// https://developer.mozilla.org/en-US/docs/Web/API/HTMLElement/dataset
+    const target = event.target;
+    const link = target.dataset.link;
+    if(link == null) {
+        return;
+    }
+    console.log(event.target.dataset.link);
+    const scrollTo = document.querySelector(link);
+    scrollTo.scrollIntoView({behavior: "smooth"});
+});
+
+// var elmnt = document.getElementById("content");
+// elmnt.scrollIntoView();
