0c8b37cba9e26bd12cdfa198767cb50d02121053
BVBFD <lsevina126@gmail.com>
2021-07-07 21:17:16 +0900

    JS: Home Contact

--- main.js
+++ main.js
@@ -26,9 +26,22 @@ navbarMenu.addEventListener('click', (event) => {
         return;
     }
     console.log(event.target.dataset.link);
-    const scrollTo = document.querySelector(link);
-    scrollTo.scrollIntoView({behavior: "smooth"});
+    // const scrollTo = document.querySelector(link);
+    // scrollTo.scrollIntoView({behavior: "smooth"});
+    scrollIntoView(link);
 });
-
 // var elmnt = document.getElementById("content");
 // elmnt.scrollIntoView();
+
+// Handle click on "contact me" button on home
+const homeContactBtn = document.querySelector('.home__contact');
+homeContactBtn.addEventListener('click', () => {
+    scrollIntoView('#contact');
+    // const scrollTo = document.querySelector('#contact');
+    // scrollTo.scrollIntoView({ behavior: 'smooth' });
+});
+
+function scrollIntoView(selector) {
+    const scrollTo = document.querySelector(selector);
+    scrollTo.scrollIntoView({ behavior: 'smooth' });
+}
